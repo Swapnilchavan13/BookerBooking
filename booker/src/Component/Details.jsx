8780971238ee82679c049cdbe60bd47341842dbe
swapnil chavan <chavanswapnil822@gmail.com>
2023-10-28 17:10:36 +0530

    updated code with upi id

--- booker/src/Component/Details.jsx
+++ booker/src/Component/Details.jsx
@@ -6,6 +6,13 @@ export const Details = () => {
   const navigate = useNavigate();
   const [savedData, setSavedData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    customerMobile: '',
+    customerName: '',
+    gender: 'Male',
+    paymentMethod: 'UPI',
+    upiRef: '',
+  });
 
   useEffect(() => {
     const savedDataString = localStorage.getItem('data');
@@ -17,14 +24,28 @@ export const Details = () => {
     }
   }, []);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   const handleConfirmClick = () => {
     if (savedData) {
+      // Combine form data with saved data
+      const bookingData = {
+        ...savedData,
+        ...formData,
+      };
+
       fetch('http://62.72.59.146:3005/bookingdata', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(savedData),
+        body: JSON.stringify(bookingData),
       })
         .then((response) => response.json())
         .then((data) => {
@@ -33,60 +54,154 @@ export const Details = () => {
             setIsModalOpen(true);
           } else {
             alert('Sorry Seats are already booked');
-            navigate('/home')
+            navigate('/home');
           }
         })
         .catch((error) => {
           alert('Error storing data:', error);
         });
+        console.log(bookingData)
     }
   };
-  
 
   const handlePrintClick = () => {
     window.print();
-    navigate('/home')
+  
+    setTimeout(() => {
+      navigate('/home');
+    }, 2000); 
   };
-  const handleCancel= () =>{
-    setIsModalOpen(false)
-    navigate('/home')
-  }
+  
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    navigate('/home');
+  };
 
   return (
     <div className="container">
-      <h1>Details</h1>
+      <h1>Booking Details</h1>
       {savedData && (
         <div>
+          {/* Display saved data here */}
           <h2>Theater Name: {savedData.tname}</h2>
           <h4>Movie Name: {savedData.mname}</h4>
           <h4>Show Date: {savedData.sdate}</h4>
           <h4>Show Time: {savedData.showtime}</h4>
           <h4>Seats: {savedData.seats.join(', ')}</h4>
           <h4>Total: Rs.{savedData.seats.length * 100} /-</h4>
+          <div>
+            
+            <div>
+              <label htmlFor="customermobile"> Customer's Mobile</label>
+              <input
+                type="text"
+                name="customerMobile"
+                id="customerMobile"
+                placeholder="Customer's Mobile"
+                value={formData.customerMobile}
+                onChange={handleInputChange}
+              />
+            </div>
 
-          <img width="300px" height="350px" src="qrcode.jpeg" alt="" />
+            <div>
+              <label htmlFor="customername"> Customer's Name</label>
+              <input
+                type="text"
+                name="customerName"
+                id="customerName"
+                placeholder="Customer's Name"
+                value={formData.customerName}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              {/* <label>Gender</label> */}
+              <br />
+              <input
+                type="radio"
+                name="gender"
+                id="male"
+                value="Male"
+                checked={formData.gender === 'Male'} // Set as checked if 'Male'
+                onChange={handleInputChange}
+              />
+              <label htmlFor="male">Male</label>
+              <input
+                type="radio"
+                name="gender"
+                id="female"
+                value="Female"
+                checked={formData.gender === 'Female'}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="female">Female</label>
+            </div>
+
+            <div>
+              <br />
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="upi"
+                value="UPI"
+                checked={formData.paymentMethod === 'UPI'}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="upi">UPI</label>
+              <input
+                type="radio"
+                id="cash"
+                value="Cash"
+                name="paymentMethod"
+                checked={formData.paymentMethod === 'Cash'} 
+                onChange={handleInputChange}
+              />
+              <label htmlFor="cash">Cash</label>
+            </div>
+          </div>
           <br />
-          <button onClick={handleConfirmClick}>Confirm And Pay</button>
+          <div className={formData.paymentMethod === 'Cash' ? 'scanner hidden' : 'scanner'}>
+        <h3>Scan And Pay The Amount</h3>
+        <img width="300px" height="360px" src="qrcode.jpeg" alt="" />
+        <br />
+        <div>
+              <label htmlFor="customername"> UPI Ref. No</label>
+              <input
+                type="text"
+                name="upiRef"
+                id="upiRef"
+                placeholder="UPI Ref. No"
+                value={formData.upiRef}
+                onChange={handleInputChange}
+              />
+        </div>
+      </div>
+
+          <br />
+          <button onClick={handleConfirmClick}>Book Ticket</button>
+          <button style={{marginLeft:'20px'}} onClick={handleCancel}>Cancel</button>
+
         </div>
       )}
 
-{isModalOpen && (
-  <div className="popup">
-    <div className="popup-content">
-      <h2>Ticket Booking Details</h2>
-      <h4>Theater Name: {savedData.tname}</h4>
-      <h4>Movie Name: {savedData.mname}</h4>
-      <h4>Show Date: {savedData.sdate}</h4>
-      <h4>Show Time: {savedData.showtime}</h4>
-      <h4>Seats: {savedData.seats.join(', ')}</h4>
-      <h4>Total: Rs.{savedData.seats.length * 100} /-</h4>
+      {isModalOpen && (
+        <div className="popup">
+          <div className="popup-content">
+            <h2>Ticket Booking Details</h2>
+            <h4>Theater Name: {savedData.tname}</h4>
+            <h4>Movie Name: {savedData.mname}</h4>
+            <h4>Show Date: {savedData.sdate}</h4>
+            <h4>Show Time: {savedData.showtime}</h4>
+            <h4>Seats: {savedData.seats.join(', ')}</h4>
+            <h4>Total: Rs.{savedData.seats.length * 100} /-</h4>
 
-      <button onClick={handlePrintClick}>Print Ticket</button> <br />
-      <button onClick={handleCancel}>Cancel</button>
-    </div>
-  </div>
-)}
-
+            <button onClick={handlePrintClick}>Print Ticket</button> <br />
+            <button onClick={handleCancel}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
